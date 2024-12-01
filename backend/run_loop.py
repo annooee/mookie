5c02269e538739f1d4a5f01001e6fee05bbc2854
Annie-Yu <Annie.Yu@ibm.com>
2024-12-01 04:25:16 -0500

    run loop now can get curr playing and connect to pi

--- backend/run_loop.py
+++ backend/run_loop.py
@@ -1,8 +1,12 @@
 import time
 from langchain_integration import ImageToPlaylistChain  # Assuming langchain_integration.py is the name of the file with the class
 from capture_send import trigger_capture_and_send  # Import the function to trigger capture and send
+import json
 
 image_path = '../images/image.png'  # This is the static path for the image
+with open("access_token.json", "r") as f:
+    token_data = json.load(f)
+    access_token = token_data.get("access_token")
 
 def main_loop():
     for i in range(5):
@@ -12,7 +16,7 @@ def main_loop():
         trigger_capture_and_send()
 
         # Create an instance of the ImageToPlaylistChain class and run the main method
-        chain = ImageToPlaylistChain(image_path)
+        chain = ImageToPlaylistChain(image_path,access_token)
         chain.main()
 
         print(f"Iteration {i + 1} completed.\n")
